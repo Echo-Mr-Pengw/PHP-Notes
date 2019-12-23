1282e262c0ce05bf6a6d8c8dc381b3a482cce04e
pengw <pengw@heitao.com>
2019-12-23 19:55:48 +0800

    PHP7 类常量与接口常量的比较

---
+++ 类与对象/classConstants.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHP7 类常量
+ * @author new1024kb
+ * @since  2019-12-23
+ */
+
+class Student {
+	const NAME = 'new1024kb';
+}
+
+echo Student::NAME;   //输出：new2014kb
+
+
+class Sunny extends Student {
+	const NAME = 'sunny new1024kb';
+}
+
+echo Sunny::NAME;    //输出：sunny new1024kb
