3e184c89ed33d54ed3cd990a78120bd8ae53c2ee
pengw <pengw@heitao.com>
2019-12-25 10:50:06 +0800

    PHP7 构造函数初始化

---
+++ 类与对象/构造函数和析构函数/constructFunction01.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * PHP7 构造函数
+ * 实例化时，自动调用构造函数，一般用于做初始化工作
+ * @author  pengw
+ * @since  2019-12-25
+ */
+class ConstructFunction {
+	public function __construct() {
+		echo 'init';
+	}
+}
+
+new ConstructFunction();  //输出：init
