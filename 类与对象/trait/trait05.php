db7644eee3e63bc6c0bb97e4e0879d14925499d6
pengw <2943927631@qq.com>
2020-01-06 10:52:30 +0800

    增加别名实例

--- 类与对象/trait/trait05.php
+++ 类与对象/trait/trait05.php
@@ -22,9 +22,9 @@ echo $t->say();  //输出：Demo say()
 
 
 class PrivateTrait05 {
-	//将say()方法的访问控制修改成private
 	use Demo {
-		say as private;
+		say as private;          //将say()方法的访问控制修改成private
+		say as public pubsay;    //给say()方法起一个别名pubsay，其访问控制为public。不影响访问控制为private的say方法
 	}
 }
 
@@ -32,4 +32,5 @@ $pt = new PrivateTrait05();
 //PHP Fatal error:  Uncaught Error: Call to private method PrivateTrait05::say() from context '' in xxx\person-git\PHP-Notes\类与对象\trait\trait05.php:33
 echo $pt->say();
 
+echo $pt->pubsay();  //输出：Demo say()
 
