6e70ffd52e32d5621a779f1c59272588dbe9789c
pengw <2943927631@qq.com>
2020-06-14 23:18:25 +0800

    private修饰的构造方法

--- 类与对象/构造函数和析构函数/constructFunction03.php
+++ 类与对象/构造函数和析构函数/constructFunction03.php
@@ -1,7 +1,7 @@
 <?php 
 /**
  * PHP7 构造函数
- * 如果父类中有构造函数，子类没有构造函数且继承了父类，子类实例化时，会自动调到父类中的构造函数。
+ * 如果父类中有构造函数，子类没有构造函数且继承了父类(父类的构造方法 不被private修饰的时候)，子类实例化时，会自动调到父类中的构造函数。
  * @author new1024kb
  * @since  2019-12-25
  */
@@ -14,6 +14,11 @@ class ParentConstructFunction {
 	public function __construct() {
 		echo 'parent init';
 	}
+
+	// PHP Fatal error:  Uncaught Error: Call to private ParentConstructFunction::__construct() from invalid context 
+	/*private function __construct() {
+		echo 'private parent init';
+	}*/
 }
 
 class ChildConstructFunction extends ParentConstructFunction { 
